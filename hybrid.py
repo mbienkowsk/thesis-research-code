44e2ca793cbabdf62fdcec6a5024d8dc1d10931f
Maksym <bienkowski.maksym@gmail.com>
2025-03-15 19:24:14 +0100

    remove hybrid, rename hybrid2 to hybrid

--- hybrid.py
+++ hybrid.py
@@ -1,28 +1,13 @@
 from loguru import logger
-import seaborn as sns
 import cma
-import numpy as np
+from scipy.optimize import golden
 from constants import PLOT_PATH
-from funs import Elliptic, OptFun, Rastrigin, Rosen
-from scipy.optimize import bracket, golden
-from codetiming import Timer
-from dataclasses import dataclass
-from matplotlib import pyplot as plt
+from funs import Elliptic, OptFun, Rastrigin, Rosen, Sphere
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from util import distance_from_optimum
-
-
-rng = np.random.default_rng(0)
-
-DIMENSIONS = 30
-START_SIGMA = 1
-BOUNDS = 3
-
-
-@dataclass
-class SingleResult:
-    fun_val: np.float64
-    dist_from_opt: np.float64
 
 
 def one_dim(fun, x, d):
@@ -35,94 +20,107 @@ def one_dim(fun, x, d):
     return wrapper
 
 
-def single_run(
-    fun: OptFun, stop_after: int, popsize: int | None = None
-) -> SingleResult:
+DIMENSIONS = 100
+BOUNDS = 3
+
+rng = np.random.default_rng(0)
+
+
+def single_run(fun: OptFun, popsize: int | None = None):
     x = (rng.random(DIMENSIONS) - 0.5) * 2 * BOUNDS
+    es = cma.CMAEvolutionStrategy(x, 1, inopts={"popsize": popsize} if popsize else {})
+    es.optimize(fun.fun)
 
-    inopts = {"popsize": popsize} if popsize is not None else {}
+    max_cutoff = int(es.countiter * 0.5)
+    step = 50
+    cutoffs = range(10, max_cutoff, 10)
+    logger.info("Cutoffs: {}", cutoffs)
 
-    es = cma.CMAEvolutionStrategy(x, START_SIGMA, inopts=inopts)
-    print(es.popsize)
+    # reinitialize
+    x = (rng.random(DIMENSIONS) - 0.5) * 2 * BOUNDS
+    es = cma.CMAEvolutionStrategy(x, 1, inopts={"popsize": popsize} if popsize else {})
+    midpoint_distances, midpoint_values, golden_distances, golden_values = (
+        [],
+        [],
+        [],
+        [],
+    )
 
-    logger.info("Starting optimization using CMA-ES")
-
-    for i in range(stop_after):
+    for i in range(cutoffs[-1] + 1):
         if es.stop():
+            logger.info(f"Early stopping after {i} iterations")
             break
         es.tell(*es.ask_and_eval(fun.fun))
 
-    logger.info(
-        f"Finished optimization using CMA-ES (stop after {stop_after}, popsize {popsize})"
-    )
+        if i in cutoffs:
+            d = es.C @ fun.grad(es.mean)
+            solution, fval, funcalls = golden(
+                one_dim(fun, es.mean, d), full_output=True
+            )
+            logger.warning(f"Gradient value: {np.linalg.norm(fun.grad(es.mean))}")
+            print(f"Golden solution: {solution} for cutoff {i}")
+            # logger.info(f"Golden search took {funcalls} function calls at cutoff {i}")
 
-    d = es.C @ fun.grad(es.mean)
+            golden_solution = es.mean + solution * d
+            actual_optimum = np.ones(DIMENSIONS) * fun.optimum
 
-    one_dim_fun = one_dim(fun, es.mean, d)
-    solution, fval, funcalls = golden(one_dim_fun, full_output=True)
+            golden_values.append(fun.fun(golden_solution))
+            golden_distances.append(np.linalg.norm(golden_solution - actual_optimum))
 
-    print(f"Value in optimum: {fval}")
-    print(f"Function calls in optimum: {funcalls}")
+            midpoint_values.append(fun.fun(es.mean))
+            midpoint_distances.append(np.linalg.norm(es.mean - actual_optimum))
 
-    multidim_solution = es.mean + solution * d
-
-    fun_val = fun.fun(multidim_solution)
-    actual_optimum = np.ones(DIMENSIONS) * fun.optimum
-    dist_from_opt = np.linalg.norm(multidim_solution - actual_optimum)
-
-    print(f"Function value at the optimum from golden: { fun_val }")
-    print("Distance of the solution from optimum: ", dist_from_opt)
-
-    return SingleResult(fun_val, dist_from_opt)
-
-
-def main():
-    popsizes = (None, DIMENSIONS, 2 * DIMENSIONS, 4 * DIMENSIONS)
-    stop_values = range(100, 2001, 200)
-    FUN = Rosen
-
-    results = {
-        popsize: {
-            stop_value: single_run(Rosen, stop_value, popsize)
-            for stop_value in stop_values
-        }
-        for popsize in popsizes
-    }
-
-    save_dir = PLOT_PATH / "hybrid" / FUN.name / str(DIMENSIONS)
+    save_dir = PLOT_PATH / "hybrid" / fun.name / str(DIMENSIONS)
     save_dir.mkdir(parents=True, exist_ok=True)
 
-    for popsize in popsizes:
-        losses = np.array([results[popsize][stop].fun_val for stop in stop_values])
-        dists = np.array([results[popsize][stop].dist_from_opt for stop in stop_values])
-        x = np.array(stop_values)
+    x = np.array(cutoffs)
 
-        fig, axes = plt.subplots(1, 2)
+    fig, axes = plt.subplots(1, 2)
+    plt.suptitle(f"{fun.name}: lambda={popsize}, dim={DIMENSIONS}")
 
-        popsize = popsize if popsize is not None else 14  # TODO: take from cma
-        plt.suptitle(f"Population size: {popsize} in {DIMENSIONS} dimensions")
+    print(x.shape, len(golden_values), len(midpoint_values))
+    sns.lineplot(
+        x=x,
+        y=golden_values,
+        ax=axes[0],
+        label="Golden search",
+    )
+    sns.lineplot(
+        x=x,
+        y=midpoint_values,
+        ax=axes[0],
+        label="CMA-ES midpoint",
+    )
 
-        sns.lineplot(
-            x=x,
-            y=losses,
-            ax=axes[0],
-        )
-        axes[0].set_yscale("log")
-        axes[0].set_xlabel("Iterations")
-        axes[0].set_ylabel("Loss")
+    axes[0].set_yscale("log")
+    axes[0].set_xlabel("Cutoff/iterations")
+    axes[0].set_ylabel("Loss")
+    axes[0].set_yscale("log")
+    axes[0].set_title("Function value at the solution")
 
-        sns.lineplot(
-            x=x,
-            y=dists,
-            ax=axes[1],
-        )
-        axes[1].set_yscale("log")
-        axes[0].set_xlabel("Iterations")
-        axes[0].set_ylabel("Distance from optimum")
+    sns.lineplot(
+        x=x,
+        y=golden_distances,
+        ax=axes[1],
+        label="Golden search",
+    )
+    sns.lineplot(
+        x=x,
+        y=midpoint_distances,
+        ax=axes[1],
+        label="CMA-ES midpoint",
+    )
 
-        plt.savefig(save_dir / f"popsize_{popsize}.png")
-        plt.show()
+    axes[1].set_yscale("log")
+    axes[1].set_xlabel("Cutoff/iterations")
+    axes[1].set_ylabel("Distance")
+    axes[1].set_yscale("log")
+    axes[1].set_title("Distance from the optimum")
+    plt.savefig(save_dir / f"popsize_{popsize}.png")
+    # plt.show()
 
 
 if __name__ == "__main__":
-    main()
+    for fun in (Sphere,):
+        for size in (120,):
+            single_run(fun, size)
