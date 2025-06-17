56eb9b87f2b5b0b08aee1c9e5af03f1090caecfa
Maksym <bienkowski.maksym@gmail.com>
2025-06-18 00:09:13 +0200

    feat: initial all_hybrids comp completed

--- experiments/all_hybrids/all_hybrids.py
+++ experiments/all_hybrids/all_hybrids.py
@@ -1,32 +1,42 @@
 """Since es.C approximates the inverse of the hessian matrix, test how plugging it into
 a gradient method such as BFGS compares to the vanilla CMA-ES and vanilla BFGS/L-BFGS"""
 
+import multiprocessing as mp
+import os
+import shutil
 from pathlib import Path
 from typing import cast
 
+import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 from cma.evolution_strategy import CMAEvolutionStrategy
 from loguru import logger
 from scipy.optimize import bracket, golden, minimize
+from sympy import prime
 
 from lib.funs import Elliptic
 from lib.lincmaes import CMAVariation
 from lib.util import (BestValueEvalCounterCallback,
                       BFGSBestValueEvalCounterCallback, CMAExperimentCallback,
-                      EvalCounter, gradient_central)
+                      EvalCounter, gradient_central,
+                      load_and_interpolate_results)
 from lib.wrapper import eswrapper
 
-BOUNDS = 128
+BOUNDS = 100
 DIM = 10
 MAXEVALS = 4000 * DIM
-RESULT_DIR = Path(f"results/dim_{DIM}")
+K_VALUE = 1
+SWITCH_AFTER = K_VALUE * DIM
+RESULT_DIR = Path(f"results/dim_{DIM}/K_{K_VALUE}")
 NUM_RUNS = 25
 VANILLA_RESULT_DIR = RESULT_DIR / "vanilla"
 BFGS_RESULT_DIR = RESULT_DIR / "bfgs"
 LBFGS_RESULT_DIR = RESULT_DIR / "lbfgs"
 LINESEARCH_RESULT_DIR = RESULT_DIR / "linesearch"
 CMABFGS_RESULT_DIR = RESULT_DIR / "cmabfgs"
-CMALBFGS_RESULT_DIR = RESULT_DIR / "cmabfgs"
+
+PLOT_X_CUTOFF = 200
 
 
 def run_vanilla(x: np.ndarray, seed: int, idx: int):
@@ -161,3 +171,61 @@ def run_cma_bfgs(x: np.ndarray, seed: int, idx: int, switch_after: int):
             delimiter=",",
             header="evals, best",
         )
+
+
+def plot_results(result_path: str):
+
+    fig = plt.figure()
+    postscripts = ["vanilla", "bfgs", "lbfgs", "linesearch", "cmabfgs"]
+
+    label_to_dirs = {
+        "vanilla CMA-ES": VANILLA_RESULT_DIR,
+        "vanilla BFGS": BFGS_RESULT_DIR,
+        "vanilla L-BFGS": LBFGS_RESULT_DIR,
+        "CMA-ES + linesearch": LINESEARCH_RESULT_DIR,
+        "CMA-ES + BFGS": CMABFGS_RESULT_DIR,
+    }
+
+    for label, dir in label_to_dirs.items():
+        x, y = load_and_interpolate_results(str(dir))
+        sns.lineplot(
+            x=x,
+            y=y,
+            label=label,
+            ax=fig.gca(),
+        )
+
+    plt.title(f"Funkcja pokrzywiona w {DIM} wymiarach")
+    plt.xlabel("Liczba ewaluacji f. celu")
+    plt.ylabel("Najlepsze znalezione rozwiązanie")
+    plt.yscale("log")
+    plt.savefig(RESULT_DIR / f"plot.png")
+
+
+def single_run(idx: int):
+    seed: int = prime(idx)  # pyright: ignore[reportAssignmentType]
+    rng = np.random.default_rng(seed)
+    x = (rng.random(DIM) - 0.5) * 2 * BOUNDS
+
+    run_vanilla(x, seed, idx)
+    run_bfgs(x, seed, idx)
+    run_lbfgs(x, seed, idx)
+    run_linesearch(x, seed, idx, SWITCH_AFTER)
+    run_cma_bfgs(x, seed, idx, SWITCH_AFTER)
+
+
+def main():
+    with mp.Pool(mp.cpu_count()) as pool:
+        pool.map(single_run, range(1, NUM_RUNS + 1))
+
+
+if __name__ == "__main__":
+    if os.path.exists(RESULT_DIR):
+        shutil.rmtree(RESULT_DIR)
+
+    os.makedirs(RESULT_DIR, exist_ok=True)
+    os.makedirs(RESULT_DIR / "bfgs", exist_ok=True)
+    os.makedirs(RESULT_DIR / "cma", exist_ok=True)
+
+    main()
+    plot_results(str(RESULT_DIR))
